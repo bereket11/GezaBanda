dcfb7eb11d114659f019975714db30c84e70ff6a
bereket11 <bhaile@ucsc.edu>
2019-09-22 08:35:11 -0700

    Banda Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GezaBanda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
